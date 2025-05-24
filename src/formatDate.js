35c288d77634a9166744e9aaa22cb088e54c767a
Alexandre Almeida <alexandredd@duck.com>
2025-05-24 17:01:48 -0300

    Add task solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,37 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const oldDate = date.split(fromFormat[3]);
+  const day = oldDate[fromFormat.indexOf('DD')];
+  const month = oldDate[fromFormat.indexOf('MM')];
+  const oldIndexYear = fromFormat.includes('YYYY')
+    ? fromFormat.indexOf('YYYY')
+    : fromFormat.indexOf('YY');
+  const newIndexYear = toFormat.includes('YYYY')
+    ? toFormat.indexOf('YYYY')
+    : toFormat.indexOf('YY');
+
+  const newFormat = [];
+
+  newFormat[toFormat.indexOf('DD')] = day;
+  newFormat[toFormat.indexOf('MM')] = month;
+
+  if (fromFormat[oldIndexYear] === toFormat[newIndexYear]) {
+    newFormat[newIndexYear] = oldDate[oldIndexYear];
+
+    return newFormat.join(toFormat[3]);
+  }
+
+  if (fromFormat[oldIndexYear].length === 4) {
+    newFormat[newIndexYear] = oldDate[oldIndexYear].slice(2);
+  } else {
+    newFormat[newIndexYear] =
+      oldDate[oldIndexYear] < 30
+        ? `20${oldDate[oldIndexYear]}`
+        : `19${oldDate[oldIndexYear]}`;
+  }
+
+  return newFormat.join(toFormat[3]);
 }
 
 module.exports = formatDate;
